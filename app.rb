54ec228aa7d3d9c4df5c3385bb0b36faa14e769a
Katie Fritz <katie@katiemfritz.com>
2015-11-17 19:03:50 -0500

    pluralize method works

--- app.rb
+++ app.rb
@@ -5,4 +5,14 @@ class MyWebApp < Sinatra::Base
     erb :home
   end
 
+	helpers do
+
+		end
+		def pluralize(count, singular_name)
+			if count == 1
+				"1 #{singular_name}"
+			else
+				"#{count} #{singular_name}s"
+		end
+	end
 end
