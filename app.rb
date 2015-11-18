86f78094ddc6c8931af3435f3755623445f5ca22
Katie Fritz <katie@katiemfritz.com>
2015-11-17 19:19:15 -0500

    use_google_font(font) works

--- app.rb
+++ app.rb
@@ -7,12 +7,20 @@ class MyWebApp < Sinatra::Base
 
 	helpers do
 
-		end
 		def pluralize(count, singular_name)
 			if count == 1
 				"1 #{singular_name}"
 			else
 				"#{count} #{singular_name}s"
+			end
 		end
+
+		def use_google_font(font)
+			%(
+				<link href='https://fonts.googleapis.com/css?family=#{font}' rel='stylesheet' type='text/css'>
+				<style>body{font-family: #{font}, sans-serif;}</style>
+			)
+		end
+
 	end
 end
