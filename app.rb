d9a20c80fdbffd7f7a76c9e989c182dade313c0c
Katie Fritz <katie@katiemfritz.com>
2015-12-06 14:38:56 -0500

    fixed indentation in app.rb?

--- app.rb
+++ app.rb
@@ -5,30 +5,30 @@ class MyWebApp < Sinatra::Base
     erb :home
   end
 
-	helpers do
+  helpers do
 
-		def pluralize(count, singular_name)
-			if count == 1
-				"1 #{singular_name}"
-			else
-				"#{count} #{singular_name}s"
-			end
-		end
+    def pluralize(count, singular_name)
+      if count == 1
+        "1 #{singular_name}"
+      else
+        "#{count} #{singular_name}s"
+      end
+    end
 
-		def use_google_font(font)
-			%(
-				<link href='https://fonts.googleapis.com/css?family=#{font}' rel='stylesheet' type='text/css'>
-				<style>body{font-family: #{font}, sans-serif;}</style>
-			)
-		end
+    def use_google_font(font)
+      %(
+        <link href='https://fonts.googleapis.com/css?family=#{font}' rel='stylesheet' type='text/css'>
+        <style>body{font-family: #{font}, sans-serif;}</style>
+      )
+    end
 
-		def icon_button(icon, message='', type='default')
-			%(
-			<button type="#{type}" class="btn btn-default" aria-label="Left Align">
-  			<span class="glyphicon glyphicon-#{icon}" aria-hidden="true">#{message}</span>
-			</button>
-			)
-		end
+    def icon_button(icon, message='', type='default')
+      %(
+	      <button type="#{type}" class="btn btn-default" aria-label="Left Align">
+	        <span class="glyphicon glyphicon-#{icon}" aria-hidden="true">#{message}</span>
+	      </button>
+      )
+    end
 
-	end
+  end
 end
